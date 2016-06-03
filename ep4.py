8deff96efb46d75f4cbedcff95b62ea73974c132
Renan Tiago dos Santos Silva <renan.tssilva@gmail.com>
2016-06-03 12:09:18 -0300

    add simulaQuad function

--- ep4.py
+++ ep4.py
@@ -16,7 +16,35 @@ def leEntrada(nome):
     return grade, pares
     f.close()
 
+def simulaQuad(n,m,lista, t):
+    lista_tmp = []
+    for i in range(t):
+        for j in range(n):
+            for k in range(m):
+                
+                vizinhos = 0
+                #print(j,k, " vizinhos:")
+                for l in range(j-1, j+2):
+                    for o in range(k-1, k+2):
+
+                        if (l!=j or o!=k) and (l%n,o%m) in lista:
+                            #print(l%n,o%m)
+                            vizinhos = vizinhos+1
+
+                if (j,k) in lista and 2<=vizinhos<=3:
+                    lista_tmp.append((j,k))
+                elif vizinhos == 3:
+                    lista_tmp.append((j,k))
+        lista = lista_tmp
+        lista_tmp = []
+    return lista
+
+
+
+
 def main():
     grade, pares = leEntrada("arquivo.txt")
+    lista = simulaQuad(4,4,pares,3)
+    print(lista)
     
 main()
